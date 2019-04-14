3ecf87ed0c94f05fc8ba3dc5e934615adac5a38b
Akshata Mohanty <akshatamohanty@gmail.com>
2019-04-14 20:49:13 +0800

    Add web files for demo

---
+++ web/script.js
@@ -0,0 +1,16 @@
+let net;
+
+async function app() {
+  // Load the base resnet model.
+  const base_resnet_model = await tf.loadLayersModel('./base_resnet/model.json', strict=false);
+  console.log('Sucessfully loaded baseresnet model');
+
+  // Load the model.
+  const extra_layers_model = await tf.loadLayersModel('./extra_layers/model.json');
+  console.log('Sucessfully loaded extra layers model');
+
+  // Make a prediction through the model on our image.
+//   const imgEl = document.getElementById('img');
+//   const result = await net.classify(imgEl);
+//   console.log(result);
+}
