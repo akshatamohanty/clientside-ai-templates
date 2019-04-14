d4f36c444f7cac6b4d91d4410692f11ff045f671
Akshata Mohanty <akshatamohanty@gmail.com>
2019-04-14 22:39:52 +0800

    Add function skeletons for web-app

--- web/script.js
+++ web/script.js
@@ -1,22 +1,49 @@
+const dog_names = []
 let base_resnet_model
 let extra_layers_model
 
-async function app() {
-  // Load the base resnet model.
-  base_resnet_model = await tf.loadLayersModel('./base_resnet/model.json', { strict: false } );
-  console.log('Sucessfully loaded baseresnet model');
+function path_to_tensor(imageEl){
+    // 3D tensor with shape (224, 224, 3)
+    let x = tf.browser.fromPixels(imageEl)
+    // convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    // TODO
+    return np.expand_dims(x, axis=0)
+}
 
-  // Load the model.
-  extra_layers_model = await tf.loadLayersModel('./extra_layers/model.json');
-  console.log('Sucessfully loaded extra layers model');
+async function extract_bottleneck(imageEl) {
+    const bottleneck_feature = await base_resnet_model.predict(tensor);
+    return bottleneck_feature
+}
+
+async function preprocess_input(tensor) {
+    // TODO: 
+    return tensor
+}
+
+// return indice of max value in list
+// Source: https://gist.github.com/engelen/fbce4476c9e68c52ff7e5c2da5c24a28
+function argmax(list) {
+    return array.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+}
+
+async function predict_breed(imageEl) {
+    const tensor = path_to_tensor(imageEl)
+    const processed_tensor = preprocess_input(tensor)
+    const bottleneck_feature = base_resnet_model.predict(processed_tensor)
+    const predicted_vector = last_layers_model.predict(bottleneck_feature)
+
+    return dog_names[ argmax(predicted_vector) ]
+}
+
+// bootstrap the app
+async function app() {
+    // Load the base resnet model.
+    base_resnet_model = await tf.loadLayersModel('./base_resnet/model.json', { strict: false } );
+    console.log('Sucessfully loaded baseresnet model');
+
+    // Load the model.
+    extra_layers_model = await tf.loadLayersModel('./extra_layers/model.json');
+    console.log('Sucessfully loaded extra layers model');
 }
 
 app()
-
-async function predict() {
-  // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const tensor = tf.browser.fromPixels(imgEl)
-  const result = await base_resnet_model.predict(tensor);
-  console.log(result);
-}
