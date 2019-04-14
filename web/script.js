56e71ac4325d8a943af63df2ece5dc043cf9a5e9
Akshata Mohanty <akshatamohanty@gmail.com>
2019-04-15 00:23:29 +0800

    Fix tensor preprocess method

--- web/script.js
+++ web/script.js
@@ -5,8 +5,8 @@ let extra_layers_model
 function path_to_tensor(imageEl){
     // 3D tensor with shape (224, 224, 3)
     let tensor = tf.browser.fromPixels(imageEl)
-            .resizeNearestNeighbor([224, 224])
-            .toFloat();
+            // .resizeNearestNeighbor([224, 224])
+            // .toFloat();
     // convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
     // const axis = 1
     // return x.expandDims(axis)
@@ -15,23 +15,12 @@ function path_to_tensor(imageEl){
 
 // Source: https://gogul09.github.io/software/mobile-net-tensorflow-js
 // Getting the 4D tensor ready for ResNet-50, and for any other pre-trained model in Keras, requires some additional processing. First, the RGB image is converted to BGR by reordering the channels. All pre-trained models have the additional normalization step that the mean pixel (expressed in RGB as $[103.939, 116.779, 123.68]$ and calculated from all pixels in all images in ImageNet) must be subtracted from every pixel in each image. This is implemented in the imported function preprocess_input. If you're curious, you can check the code for preprocess_input here.
-
-// Now that we have a way to format our image for supplying to ResNet-50, we are now ready to use the model to extract the predictions. This is accomplished with the predict method, which returns an array whose $i$-th entry is the model's predicted probability that the image belongs to the $i$-th ImageNet category. This is implemented in the ResNet50_predict_labels function below.
-
-// By taking the argmax of the predicted probability vector, we obtain an integer corresponding to the model's predicted object class, which we can identify with an object category through the use of this dictionary.
 async function preprocess_input(tensor, modelName) {
-  // if model is not available, send the tensor with expanded dimensions
-//   if (modelName === undefined) {
-//     return tensor.expandDims();
-//   } 
-
-  // if model is mobilenet, feature scale tensor image to range [-1, 1]
-  // else if (modelName === "mobilenet") {
-    let offset = tf.scalar(127.5);
-    return tensor.sub(offset)
-      .div(offset)
+    // TODO: let reversed_tensor = tensor.reve
+    let offset = tf.tensor1d([103.939, 116.779, 123.68]);
+    return tensor
+      .sub(offset)
       .expandDims();
-  //} 
 }
 
 // return indice of max value in list
@@ -68,8 +57,9 @@ async function predict_breed() {
     const predicted_vector = extra_layers_model.predict(bottleneck_feature)
 
     // print_prediction(predicted_vector) 
-    console.log(dog_names[ argmax(await predicted_vector.data()) ])
-    const dog_breed = dog_names[ argmax(await predicted_vector.data()) ]
+    const dogIndex = argmax(await predicted_vector.data())
+    const dog_breed = dog_names[ dogIndex ]
+    console.log(dog_breed, dogIndex)
     document.getElementById('console').innerText = dog_breed 
 }
 
