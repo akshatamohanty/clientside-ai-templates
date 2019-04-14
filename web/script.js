2f0f6b3279f319388208a1347775818e357f7b00
Akshata Mohanty <akshatamohanty@gmail.com>
2019-04-14 22:24:06 +0800

    Fix basenet model loading bug

--- web/script.js
+++ web/script.js
@@ -1,16 +1,22 @@
-let net;
+let base_resnet_model
+let extra_layers_model
 
 async function app() {
   // Load the base resnet model.
-  const base_resnet_model = await tf.loadModel('./base_resnet/model.json', strict=false);
+  base_resnet_model = await tf.loadLayersModel('./base_resnet/model.json', { strict: false } );
   console.log('Sucessfully loaded baseresnet model');
 
   // Load the model.
-  const extra_layers_model = await tf.loadLayersModel('./extra_layers/model.json');
+  extra_layers_model = await tf.loadLayersModel('./extra_layers/model.json');
   console.log('Sucessfully loaded extra layers model');
+}
 
+app()
+
+async function predict() {
   // Make a prediction through the model on our image.
-//   const imgEl = document.getElementById('img');
-//   const result = await net.classify(imgEl);
-//   console.log(result);
+  const imgEl = document.getElementById('img');
+  const tensor = tf.browser.fromPixels(imgEl)
+  const result = await base_resnet_model.predict(tensor);
+  console.log(result);
 }
